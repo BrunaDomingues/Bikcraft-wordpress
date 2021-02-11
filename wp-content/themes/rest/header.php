f030102cac40b399340a831936840307815d1a6f
BrunaDomingues <brunadomingues.dev@gmail.com>
2021-02-11 09:22:25 -0300

    criando o tema

---
+++ wp-content/themes/rest/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <ul>
+            <li class="current_page_item"><a href="index.html">Menu</a></li>
+            <li><a href="sobre.html">Sobre</a></li>
+            <li><a href="contato.html">Contato</a></li>
+        </ul>
+    </nav>
+
+    <h1><img src="<?php echo get_stylesheet_directory_uri();?>/img/rest.png" alt="Rest"></h1>
+
+    <p>Rua Marechal 29 – Copacabana – Rj</p>
+    <p class="telefone">2422-9201</p>
+</header>
