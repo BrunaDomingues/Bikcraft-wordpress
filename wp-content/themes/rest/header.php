874ccd14bec1c0b22b0cba0d294f880daeeb3052
BrunaDomingues <brunadomingues.dev@gmail.com>
2021-02-11 11:18:47 -0300

    Arrumando header e footer

--- wp-content/themes/rest/header.php
+++ wp-content/themes/rest/header.php
@@ -1,14 +1,28 @@
-<header>
-    <nav>
-        <ul>
-            <li class="current_page_item"><a href="index.html">Menu</a></li>
-            <li><a href="sobre.html">Sobre</a></li>
-            <li><a href="contato.html">Contato</a></li>
-        </ul>
-    </nav>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title><?php bloginfo('name'); ?></title>
 
-    <h1><img src="<?php echo get_stylesheet_directory_uri();?>/img/rest.png" alt="Rest"></h1>
+        <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css">
+        <!-- Header Wordpress -->
+            <?php wp_head(); ?>
+        <!-- Fecha Header Wordpress -->
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li class="current_page_item"><a href="index.html">Menu</a></li>
+                    <li><a href="/xampp/bikcraft/sobre">Sobre</a></li>
+                    <li><a href="/xampp/bikcraft/contato">Contato</a></li>
+                </ul>
+            </nav>
 
-    <p>Rua Marechal 29 – Copacabana – Rj</p>
-    <p class="telefone">2422-9201</p>
-</header>
+            <h1><img src="<?php echo get_stylesheet_directory_uri();?>/img/rest.png" alt="Rest"></h1>
+
+            <p>Rua Marechal 29 – Copacabana – Rj</p>
+            <p class="telefone">2422-9201</p>
+        </header>
